0ccfb2678b73cfcf3964dda322129fc3a2f8cbdd
leaqake <45873082+leaqake@users.noreply.github.com>
2020-08-26 13:42:47 +0300

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import time
-net = cv2.dnn.readNet("yolov3_training_2500.weights","yolov3_testing.cfg")
+net = cv2.dnn.readNet("yolov3_training_last.weights","yolov3_testing.cfg") #Use your own trained weight file or use mine. Its given in the comments.
 layer_names = net.getLayerNames()
 outputlayers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
